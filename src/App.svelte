2c5790b473596818e6c123477c3846a9e19d3b76
Risyandi <Risyandi.94@gmail.com>
2020-09-01 08:04:20 +0700

    adding example debug svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,20 @@
 <script type="text/javascript">
 	let name = "abraham lincoln";
 	let src = "./images/abraham-lincoln.png"; 
+	let user = {
+		firstName: 'Ris',
+		lastName: 'yandi'
+	}
 </script>
-<b>{name}</b>
+
+<!-- print svelte -->
+<b>{name}</b> <br/>
 <img {src} alt="abraham lincoln" style="width:200px; height:200px;">
+
+
+<br/>
+<!-- debug svelte -->
+<input bind:value={user.firstName} type="text">
+<input bind:value={user.lastName} type="text">
+{@debug user}
+<h1>Hello {user.firstName + user.lastName}</h1>
